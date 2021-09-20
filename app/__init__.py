52321850c006eda81f258072ebb762569cf6a1ce
Erick Martins Ratamero <erickmartins@gmx.net>
2021-09-20 09:50:34 -0400

    creating subframes for display

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,13 @@ def create_plot(folder):
     df_time = get_dataframe(Path(folder) / 'timings.csv')
     df_status = get_dataframe(Path(folder) / 'status.csv')
     df_sessions = get_dataframe(Path(folder) / 'sessions.csv')
+    lastmonth = datetime.today() - timedelta(weeks=2)
+    df_time['timestamp'] = pd.to_datetime(df_time['timestamp'])
+    df_status['timestamp'] = pd.to_datetime(df_status['timestamp'])
+    df_sessions['timestamp'] = pd.to_datetime(df_sessions['timestamp'])
+    display_time = df_time[df_time['timestamp']>lastmonth]
+    display_status = df_status[df_status['timestamp']>lastmonth]
+    display_sessions = df_sessions[df_sessions['timestamp']>lastmonth]
     fig = make_subplots(
         rows=3, cols=2,
         specs=[[{'colspan': 2}, None], [{}, {}], [{}, {}]],
@@ -36,36 +43,35 @@ def create_plot(folder):
     status_dic = {'green': "All systems operational",
                   'orange': "At least one API/service unresponsive",
                   'red': "All systems unresponsive"}
-    fig.add_bar(x=df_time['timestamp'], y=df_time['blitz_api'],
-                marker_color=df_status['color'], row=1,
+    fig.add_bar(x=display_time['timestamp'], y=display_time['blitz_api'],
+                marker_color=display_status['color'], row=1,
                 col=1, width=60*50*1000,
-                text=[status_dic[i] for i in df_status['color']],
+                text=[status_dic[i] for i in display_status['color']],
                 hovertemplate='Time: %{x}<br>' +
                               'Blitz API response time: %{y} s<br>' +
                               'Status: %{text}' +
                               '<extra></extra>'
                 )
-    fig.add_bar(x=df_sessions['timestamp'], y=df_sessions['sessions'],
+    fig.add_bar(x=display_sessions['timestamp'], y=display_sessions['sessions'],
                 row=2, col=1, width=60*50*1000*24,
                 hovertemplate='Time: %{x}<br>' +
                               'Total sessions: %{y}<br>' +
                               '<extra></extra>')
-    fig.add_bar(x=df_sessions['timestamp'], y=df_sessions['users'],
+    fig.add_bar(x=display_sessions['timestamp'], y=display_sessions['users'],
                 row=2, col=2, width=60*50*1000*24,
                 hovertemplate='Time: %{x}<br>' +
                               'Unique users: %{y}<br>' +
                               '<extra></extra>')
-    fig.add_bar(x=df_time['timestamp'], y=df_time['webpage'],
+    fig.add_bar(x=display_time['timestamp'], y=display_time['webpage'],
                 row=3, col=1, width=60*50*1000,
                 hovertemplate='Time: %{x}<br>' +
                               'Webpage response time: %{y} s<br>' +
                               '<extra></extra>')
-    fig.add_bar(x=df_time['timestamp'], y=df_time['json_api'],
+    fig.add_bar(x=display_time['timestamp'], y=display_time['json_api'],
                 row=3, col=2, width=60*50*1000,
                 hovertemplate='Time: %{x}<br>' +
                               'JSON API response time: %{y} s<br>' +
                               '<extra></extra>')
-    lastmonth = datetime.today() - timedelta(weeks=2)
     fig.update_xaxes(range=[lastmonth, datetime.now()])
     fig.update_layout(height=1200, showlegend=False)
 
